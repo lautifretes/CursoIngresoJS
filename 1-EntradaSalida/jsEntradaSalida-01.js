f01ef350dc5ca0f175b770ecef985e61fd5767a3
Lautaro Fretes <lautifretes93@gmail.com>
2023-01-31 10:35:20 -0300

    agregue mensaje en alert

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,10 @@
-/*
+/*Lautaro Fretes
+
+
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola curso");
+	alert("");
 }
 
