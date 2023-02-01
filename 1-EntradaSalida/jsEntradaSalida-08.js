11dac745de998985ee360263f63e5da47fb3dbf6
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-01 12:17:05 -0300

    agregue % para sacar resto

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,17 @@
-/*
+/*Lautaro Fretes
+
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	let dividendo;
+	let divisor;
+	let resto;
+	
+	dividendo = parseInt(document.getElementById("txtIdNumeroDividendo").value);
+	divisor = parseInt(document.getElementById("txtIdNumeroDivisor").value);
+	resto = parseInt(dividendo % divisor);
+	alert("el resto es " +resto);
 }
