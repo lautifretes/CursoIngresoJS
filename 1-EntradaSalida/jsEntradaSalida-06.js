cef733452e896d84fd9a48fc6f643ee3d41fe8b2
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-01 11:15:24 -0300

    agregue parseInt

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,21 @@
-/*
+/* Lautaro Fretes
+
+
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numeroUno;
+	let numeroDos;
+	let resultado;
+	
+	numeroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numeroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	resultado = parseInt( numeroUno + numeroDos);
+
+
+
+	alert("la suma es " + resultado);
 }
 
