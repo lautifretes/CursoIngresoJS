8d77174a305fb0e8c376fe1cc4b0762f62f2928b
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-01 13:34:08 -0300

    agregue +,-,*,/

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,66 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let numUno;
+	let numDos;
+	let resultado;
+	let mensaje;
+
+	numUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	resultado = parseInt(numUno + numDos);
+	mensaje = "La suma es " +resultado; 
+	
+
+
+
+	alert(mensaje);	
 }
 
 function restar()
 {
+	let numUno;
+	let numDos;
+	let resultado;
+	let mensaje;
+
+	numUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	resultado = parseInt(numUno - numDos);
+	mensaje = "La resta es " +resultado; 
 	
+	alert(mensaje);	
 }
 
 function multiplicar()
 { 
+	let numUno;
+	let numDos;
+	let resultado;
+	let mensaje;
+
+	numUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	resultado = numUno * numDos;
+	mensaje = "La multiplicacion es " +resultado; 
+	
+	alert(mensaje);	
 	
 }
 
 function dividir()
 {
+	let numUno;
+	let numDos;
+	let resultado;
+	let mensaje;
+
+	numUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	resultado = numUno / numDos;
+	mensaje = "El resultado es " +resultado; 
+	
+	alert(mensaje);	
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -6,12 +6,11 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	let dividendo;
-	let divisor;
-	let resto;
-	
-	dividendo = parseInt(document.getElementById("txtIdNumeroDividendo").value);
-	divisor = parseInt(document.getElementById("txtIdNumeroDivisor").value);
-	resto = parseInt(dividendo % divisor);
+		
+	let dividendo = parseInt(document.getElementById("txtIdNumeroDividendo").value);
+	let divisor = parseInt(document.getElementById("txtIdNumeroDivisor").value);
+	/*cuando se unsa % , * , / no hace falta parentesis 
+	 ej: let resto = parseInt(dividendo % divisor); */
+	 let resto = dividendo % divisor
 	alert("el resto es " +resto);
 }
