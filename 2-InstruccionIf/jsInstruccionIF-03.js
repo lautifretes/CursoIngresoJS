3bacf7e9d910c5518b41acb363b2d090375a889f
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-07 10:04:05 -0300

    agregue nombre y apellido

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,3 +1,8 @@
+
+/*            LAUTARO FRETES           		 */
+
+
+
 /* Enunciado:
 Al ingresar una edad debemos informar si la persona es mayor de edad, sino informar que es un menor de edad. */
 
