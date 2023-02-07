f2e2bb2b5bd11a2d33ad44555bd84aaf94ec9397
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-07 09:58:40 -0300

    agregue else

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,23 @@
+/* Enunciado:
+Al ingresar una edad debemos informar si la persona es mayor de edad, sino informar que es un menor de edad. */
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	let mensaje;
+	let mensajeUno;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	mensaje = "sos mayor de edad"
+	mensajeUno = "sos menor de edad"
+
+	if (edad >= 18) {
+		
+		alert(mensaje)
+
+	}else{
+		alert(mensajeUno)
+	}
+	
 
 }//FIN DE LA FUNCIÓN
