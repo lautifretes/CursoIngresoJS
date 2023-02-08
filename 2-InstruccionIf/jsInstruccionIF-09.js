9accc1d6509aa2181439655773e4a5d88c0d5251
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-08 10:27:51 -0300

    add math.ceil y random

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,26 @@
+/*      LAUTARO FRETES  */
+
+/* Enunciado:
+Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive */
+
+
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let numero;
+	let mensaje;
+	
+	numero = Math.ceil(Math.random()*10)
+	mensaje = "el numero random es " + numero
+	console.log(mensaje)
+
+	/* alert("ok"); */	
 
 }//FIN DE LA FUNCIÓN
+
+
+
+
+//    La función Math.floor() redondea al entero inferior.
+
+//    La funcion Math.ceil() redondea al entero superior.
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,38 @@
+//                      LAUTARO FRETES
+
+
+/* Enunciado:
+Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4 */
+
+
 function mostrar()
 {
+	let numero;
+	let mensaje;
+
+	numero = Math.ceil(Math.random()*10)
+
+
+
+	if (numero < 4)
+	{
+		mensaje = numero + " Vamos la proxima se puede";
+	}
+	else if (numero < 9)
+	{
+	mensaje = numero + " APROBO";
+	}
+	else
+	{
+	mensaje = numero + " EXCELENTE";
+	}
+	console.log(mensaje)
+
+
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	//alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
