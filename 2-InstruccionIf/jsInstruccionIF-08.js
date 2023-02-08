a49fdb8e0d0bc2c5238c0447a12ee20db8ff874b
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-08 09:38:37 -0300

    agregue if

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,7 +1,27 @@
+
+/*     LAUTARO FRETES */
+
+
+/* Enunciado:
+Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.' */
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad ;
+	let estadoCivil;
+	let mensaje;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	if (edad > 17 && estadoCivil == "Soltero") {
+		mensaje = "Es soltero y no es menor"
+		alert(mensaje)
+		
+	} 
+
+	
 	
 
 
