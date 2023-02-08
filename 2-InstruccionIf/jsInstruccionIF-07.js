9c2e9a45d86e7a46cb33e7482c3d12660d414d47
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-08 09:18:33 -0300

    add if

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,7 +1,27 @@
+/*                LAUTARO FRETES */
+
+
+
+
+/* Enunciado:
+Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.' */
+
+
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	let estadoCivil;
+	let mensaje;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
+	
+
+	if (edad < 18 && estadoCivil != "Soltero") {
+	mensaje ="Es muy pequeño para no ser soltero"	
+	alert(mensaje);
+	}
 	
 
 
