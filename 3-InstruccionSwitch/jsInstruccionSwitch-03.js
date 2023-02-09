9c2bd4ff129303a86f510026b8ee983ae365ca3f
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-09 12:03:22 -0300

    agregue switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,9 +1,28 @@
+//    LAUTARO FRETES
+
+/* Enunciado:
+al seleccionar un mes informar.
+si es Febrero: " Este mes no tiene más de 29 días."
+si NO es Febrero: "Este mes tiene 30 o más días" */
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAno;
+	let mensaje;
+
+	mesDelAno = document.getElementById("txtIdMes").value;	
+
+	switch (mesDelAno) {
+		case "Febrero":
+			mensaje = "Este mes no tiene más de 29 días."
+			break;
 	
+		default:
+			mensaje = "Este mes tiene 30 o más días"
+	}
+
+	alert(mensaje)
+
 	
 
 
