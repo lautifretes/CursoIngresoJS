0dab73715560124d17b9a6cd2a0123fdfb130aea
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-09 15:09:37 -0300

    agregue case y mensaje

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,38 @@
+// 				LAUTARO FRETES
+
+/* Enunciado:
+al seleccionar un mes informar.
+si es Enero: "que comiences bien el año!!!."
+si es Marzo: "a clases!!!."
+si es Julio: "se vienen las vacaciones!!!."
+si es Diciembre: "Felices fiesta!!!." */
+
+
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAno;
+	let mensaje;
 
+	mesDelAno = document.getElementById("txtIdMes").value;
+
+	switch(mesDelAno){
+		case "Enero":
+			mensaje = "que comiences bien el año!!!"
+			break;
+			case "Marzo":
+				mensaje = "a clases!!!."
+				break;
+				case "Julio":
+					mensaje = "se vienen las vacaciones!!!"
+					break;
+					case "Diciembre":
+						mensaje = "Felices fiesta!!!."
+						break;
+						default:
+							mensaje = "mes aburrido" 
+	}
+	alert(mensaje)
 
 
 }//FIN DE LA FUNCIÓN
