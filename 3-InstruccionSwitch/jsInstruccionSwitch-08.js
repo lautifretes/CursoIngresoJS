c95af3ad4e1aefa1a3e0d1accdfcf3b613150274
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-09 16:29:45 -0300

    case, destino, frio y calor

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,29 @@
+//    LAUTARO FRETES
+
+
+/* Enunciado:
+Al seleccionar un destino informar si hace FRIO o CALOR en ese destino */
+
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+	let mensaje;
+
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (destino) {
+		case "Bariloche":
+		case "Ushuaia":
+			mensaje = "Hace FRIO, abrigate."
+			break;
+		case "Cataratas":
+			mensaje = "Hace CALOR, toma mucha agua."
+			break;
+		case "Mar del plata":
+			mensaje = "Depende la epoca del año"
+			break;
+	}
+	alert(mensaje)
 
 }//FIN DE LA FUNCIÓN
