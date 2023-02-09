1e9fced20e4427d74242e39a55ce353d5fecd4a2
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-09 15:32:58 -0300

    informar dias del mes

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,35 @@
+
+//     LAUTARO FRETES
+
+/* Enunciado:
+al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días. */
+
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mes;
+	let mensaje;
 
+	mes = document.getElementById("txtIdMes").value;
 
+	switch (mes) {
+		case "Febrero":
+			mensaje = "Tiene 28 días."
+			break;
+			case "Abril":
+			case "Junio":
+			case "Septiembre":
+			case "Noviembre":
+				mensaje = "Tiene 30 días."
+			break;
+		default:
+			mensaje = "Tiene 31 días."
+			
+	}
+	
+	alert(mensaje)
 
 }//FIN DE LA FUNCIÓN
