841846b078db8ef40a27130db279866d80468fe2
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-09 18:22:18 -0300

    ejercicio 9 if y switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,72 @@
+//      LAUTARO FRETES
+
+
+/* Enunciado:
+una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base, se pide el ingreso de una estacion del año y localidad para vacacionar para poder calcular el precio final
+
+en Invierno: bariloche tiene un aumento del 20% cataratas y Cordoba tiene un descuento del 10% Mar del plata tiene un descuento del 20%
+
+en Verano: bariloche tiene un descuento del 20% cataratas y Cordoba tiene un aumento del 10% Mar del plata tiene un aumento del 20%
+
+en Otoño y Primavera: bariloche tiene un aumento del 10% cataratas tiene un aumento del 10% Mar del plata tiene un aumento del 10% y Cordoba tiene el precio sin descuento */
+
+
+
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let precio;
+	let estacionDelAño;
+	let localidad;
+	let porcentaje;
+	let precioFinal;
+	let mensaje;
+	 
+	precio = 15000
+	estacionDelAño = document.getElementById("txtIdEstacion").value;
+	localidad = document.getElementById("txtIdDestino").value;
+	porcentaje = 0
+	
+	
+	if(estacionDelAño == "Invierno") {
+		
+		switch(localidad){
+			case "Bariloche":
+				porcentaje = 20
+				break;
+			case "Mar del plata":
+				porcentaje = -20
+				break;
+			case "Cataratas":
+			case "Cordoba":
+				porcentaje = -10
+		}
+	}else if (estacionDelAño == "Verano"){
+
+		switch (localidad) {
+			case "Bariloche":
+				porcentaje = -20
+				break;
+			case "Mar del plata":
+				porcentaje = 20
+				break;
+			case "Cataratas":
+			case "Cordoba":
+				porcentaje = 10
+		}
+	}else {
+		switch (localidad) {
+			case "Bariloche":
+			case "Mar del plata":
+			case "Cataratas":
+				porcentaje = 10
+				break;
+			case "Cordoba":
+				porcentaje = 0
+		}
+	}
+	
+	precioFinal = precio + (precio * porcentaje/100)
+	mensaje = "El precio final es $" + precioFinal 
+alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,64 @@
+//    LAUTARO FRETES
+
+
+/* Enunciado:
+una agencia de viajes nos piden informar si hacemos viajes a lugares según la estación del año estemos, informar si "Se viaja" o "No se viaja" a ese lugar
+
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+
+en Otoño: Se viaja a todos los destinos.
+
+primavera: solo no se viaja a Bariloche */
+
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacionDelAnio;
+	let destino;
+	let mensaje;
+
+	estacionDelAnio = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (estacionDelAnio) {
+		case "Invierno":
+			switch(destino) {
+				case "Bariloche":
+					mensaje = "Se viaja."
+					break;
+				default:
+					mensaje = "No se viaja."
+			}
+			break;
+		case "Verano":
+			switch(destino){
+				case "Mar del plata":
+				case "Cataratas":
+					mensaje = "Se viaja."
+					break;
+				default:
+					mensaje = "Se viaja a Mar del plata y Cataratas solamente"
+			}
+			break;
+		case "Otoño":
+			switch(destino) {
+				case "Bariloche":
+				case "Cataratas":
+				case "Cordoba":
+				case "Mar del plata":
+					mensaje = "Se viaja"
+			}
+			break;
+		case "Primavera":
+			switch(destino){
+				case "Bariloche":
+					mensaje = "No se viaja"
+					break;
+				default: 
+				mensaje = "Se viaja"
+			}
+	}
+	alert(mensaje)
 
 }//FIN DE LA FUNCIÓN
