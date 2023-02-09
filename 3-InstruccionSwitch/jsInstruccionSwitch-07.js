37630415ab146d360e86ca34343089098fc74e06
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-09 16:18:56 -0300

    case,  destinos y ubicacion

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,30 @@
+//    LAUTARO FRETES
+
+
+/* Enunciado:
+Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste */
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+	let mensaje;
 
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (destino) {
+		case "Bariloche":
+			mensaje = "Se encuentra al Oeste"
+			break;
+		case "Ushuaia":
+			mensaje = "Se encuentra al Sur"
+			break;
+		case "Cataratas":
+			mensaje = "Se encuentra al Norte"
+			break;
+		case "Mar del plata":
+			mensaje = "Se encuentra al Este"
+			break;
+	}
+
+	console.log(mensaje)
 }//FIN DE LA FUNCIÓN
