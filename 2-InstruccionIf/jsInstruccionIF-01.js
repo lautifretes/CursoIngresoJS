8076793f79225d13373761c461c503645412d831
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-11 00:56:39 -0300

    ejercicio 1 bis bis

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -7,7 +7,7 @@ Al ingresar una edad que sea igual a 15, mostrar el mensaje "niña bonita". */
 
 
 //function mostrar()
-{
+//{
 
 
 	/* let edad;
@@ -22,7 +22,7 @@ Al ingresar una edad que sea igual a 15, mostrar el mensaje "niña bonita". */
 	}
 	 */
 
-}//FIN DE LA FUNCIÓN
+//}FIN DE LA FUNCIÓN
 
 
 
@@ -49,7 +49,7 @@ Al ingresar una edad que sea igual a 15, mostrar el mensaje "niña bonita". */
 	*/
 
 
-	/* Ejercicio 1bis: 
+	/* Ejercicio 1bis:   LAUTARO FRETES
 /con if
 pedir a una persona que fue de vaciones , 
 la distancia que viajo y el tiempo que tardo, para calcular la velocidad
@@ -60,7 +60,7 @@ hasta 100= buen ritmo
 hasta 120= ahi de la ley 
 mas = eso no se hace/ */
 
-function mostrar()
+/* function mostrar()
 {
 	let distancia;
 	let tiempo;
@@ -95,3 +95,62 @@ function mostrar()
 	}
 	
 }
+ */
+
+
+
+
+
+/* Ejercicio 1 bis bis:           LAUTARO FRETES
+con if
+ingresar el nombre y los datos requeridos para calcular el IMC , 
+e informar a la persona si es:
+Bajo peso <18.5
+Peso normal 18,5-24.9
+Preobesidad 25-26.9
+Obesidad I 27-29.9
+Obesidad II 30-34.9
+Obesidad III >40 */
+
+// formula para IMC = PESO dividido (altura*altura) 
+function mostrar()
+{
+	let nombre;
+	let altura;
+	let peso;
+	let imc;
+	let imcResutado;
+	let mensaje;
+
+	nombre   	= prompt("ingrese su nombre");
+	altura  	= parseFloat(prompt("ingrese su altura"));
+	peso    	= parseFloat(prompt("ingrese su peso"));
+	imc	   	    = peso / (altura * altura);
+	imcResutado	= imc.toFixed(2)
+
+	if(imcResutado < 18.5) {
+		mensaje = " Sr/Sra " + nombre + ", su imc es " + imcResutado +", bajo peso";
+		
+	}else if(imcResutado <= 24.9) {
+		mensaje = " Sr/Sra " + nombre + ", su imc es " + imcResutado +", Peso normal";
+			
+	
+	}else if(imcResutado < 26.9){
+		mensaje = " Sr/Sra " + nombre + ", su imc es " + imcResutado +", tiene preobesidad";
+		
+			
+	}else if(imcResutado < 29.9){
+		mensaje = " Sr/Sra " + nombre + ", su imc es " + imcResutado +", tiene obesidad 1";
+		
+			
+	}else if(imcResutado < 34.9){
+		mensaje = " Sr/Sra " + nombre + ", usted tiene Obesidad 2.";
+		
+	}else{
+		mensaje = " Sr/Sra " + nombre + ", su imc es " +imcResutado + ", tiene Obesidad 3.";
+		
+
+	}		
+	alert(mensaje)
+}
+
