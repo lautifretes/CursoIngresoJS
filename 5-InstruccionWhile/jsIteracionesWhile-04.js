03e3b99b587e39263cc1532c29276c6d631ff859
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-14 18:13:14 -0300

    pedir un numero entre 0 y 10

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -3,7 +3,17 @@ al presionar el botón
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
+	let numeroIngresado;
 	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	parseInt(numeroIngresado);
+	let mensaje;
+	
+	while(isNaN(numeroIngresado) || numeroIngresado == -1 || numeroIngresado > 10){
+		mensaje = "el n "+ numeroIngresado+ " no es valido"
+		alert(mensaje)
+		numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+		parseInt(numeroIngresado);
+		
+	}
 	
 }//FIN DE LA FUNCIÓN
