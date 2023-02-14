c7414cc20a35d8a29c5739e04d579f992b6f9479
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-14 17:13:22 -0300

    contador descendente

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,10 +1,16 @@
 /*
+			LAUTARO FRETES
+
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
+	let contador;
+	contador = 11; 
+
+	while(contador > 1 && contador < 12){
+		contador = contador - 1
+		alert(contador);
+	}
 
 }//FIN DE LA FUNCIÓN
