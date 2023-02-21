e8dfcb8b59451fbafe6040ab814ceaeea76513c4
Lautaro Fretes <lautifretes93@gmail.com>
2023-02-20 23:08:26 -0300

    cortar con 9

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,6 +1,29 @@
+/* 
+
+		LAUTARO FRETES
+
+Enunciado:
+al presionar el botón repetir el pedido de número hasta que ingresemos el 9.
+
+*/
+
+
+
+
+
+
 function mostrar()
 {
-	alert("ok");
+	let repeticiones;
+
+	for(i = 0; i < 100 ; i ++){
+		repeticiones = prompt("Para cortar ingrese 9");
+		repeticiones = parseInt(repeticiones);
+		if(repeticiones == 9){
+			break;
+		}
+	}
+	
 
 
 
